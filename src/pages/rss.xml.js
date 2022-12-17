34bbcb756aa16037085a9fd3194f833276a79c95
Jordi <jordi@tinybird.co>
2022-12-17 17:23:38 +0100

    Improve RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,20 @@
 import rss from '@astrojs/rss';
+import sanitizeHtml from 'sanitize-html';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
+
+// Works with Markdown files only!
+const postImportResult = import.meta.glob('./**/[^index|about]*.{md,mdx}', { eager: true });
+const posts = Object.values(postImportResult);
 
 export const get = () =>
 	rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: import.meta.env.SITE,
-		items: import.meta.glob('./**/[^index|about]*.{md,mdx}'),
+		items: posts.map((post) => ({
+			link: post.url,
+			title: post.frontmatter.title,
+			pubDate: post.frontmatter.pubDate,
+			content: sanitizeHtml(post.compiledContent()),
+		}))
 	});
